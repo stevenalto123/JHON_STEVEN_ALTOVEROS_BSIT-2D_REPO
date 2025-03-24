f23766c020c51b1a581fe285b7a08bb133937d23
stevenalto123 <steven.alto05@gmail.com>
2025-03-23 23:17:53 -0700

    Add files via upload

---
+++ Activity/Function - Activity/script.js
@@ -0,0 +1,14 @@
+
+function whatShallwear(temp) {
+    if (temp < 60) {
+        console.log("Wear a Jacket");   
+    }else if (temp < 70) {
+       console.log("Wear a Sweater");
+    }else{
+        console.log("Wear a T-shirt");
+    }
+}
+
+whatShallwear(80);
+whatShallwear(60);
+whatShallwear(50);
