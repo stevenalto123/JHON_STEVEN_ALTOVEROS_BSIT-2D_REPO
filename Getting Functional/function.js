169d4f75455d9a88f4e622042a354afc4477f3cb
stevenalto123 <steven.alto05@gmail.com>
2025-03-23 23:18:11 -0700

    Add files via upload

---
+++ Getting Functional/function.js
@@ -0,0 +1,42 @@
+// var dogName = "caminoy";
+// var dogWeight = 23;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof");
+// }
+
+// var dogName = "wayne";
+// var dogWeight = 15;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof");
+// }
+
+// var dogName = "freda";
+// var dogWeight = 50;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof");
+// }
+
+// var dogName = "brent";
+// var dogWeight = 10;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof");
+// }
+
+function bark (dogName, dogWeight) {
+    if (dogWeight > 20) {
+        console.log(dogName + " says WOOF WOOF");
+    } else {
+        console.log(dogName + " says woof woof");
+    }
+}
+
+bark("wayne", 20)
+bark("brent", 100)
