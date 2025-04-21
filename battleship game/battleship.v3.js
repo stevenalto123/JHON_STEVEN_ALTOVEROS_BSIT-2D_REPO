0daf9bf540e8c56cddf40a89bfd2f6c40ab55d6e
stevenalto123 <steven.alto05@gmail.com>
2025-04-20 23:20:38 -0700

    battleship.v3.js

--- battleship game/battleship.v3.js
+++ battleship game/battleship.v3.js
@@ -1,48 +1,45 @@
-// declaring variable
-
-var randomLoc = Math.floor(Math.random() *5);
+var randomLoc = Math.floor(Math.random() * 5);
 var shipLocations = [randomLoc, randomLoc + 1, randomLoc + 2];
+// Store ship locations in on array
 
-var guess
-var hits = 0;
-var guesses = 0;
-var isSunk = false;
+let guess;
+let hits = 0;
+let guesses = 0;
+let isSunk = false;
 
-// Game logic
+while (!isSunk) {
+    guess = prompt("Ready! , Aim! , Fire! (Enter a number 0-6): ");
 
-while (isSunk == false) {
-    guess = prompt("Ready!. Aim!. Fire! (enter number 0-6)");
-
-    if (guess == null) {
+    if (guess === null) {
         alert("Thank you for playing!");
         break;
     }
 
     guess = parseInt(guess, 10);
-    //Convert input to a number base 10
+    // Convert input to a number base 10
 
     if (isNaN(guess) || guess < 0 || guess > 6) {
-        alert("Please enter a valid number between and 6!");
+        alert("Please enter a valid number between 0 and 6!");
         continue;
-        // skip the code
+        // Skip the code
     }
-    
     guesses++;
 
     const hitIndex = shipLocations.indexOf(guess);
-   
-    if (hitIndex !== -1) { // -1 means not  found
-        alert ("Hits!");
+
+    if(hitIndex !== -1) {// -1 means not found
+        alert("Hit!");
         shipLocations.splice(hitIndex, 1);
         hits++;
-        if (shipLocations.length == 0) {
-            inSunk = true;
+        if (shipLocations.length === 0) {
+            isSunk = true;
         }
     } else {
-        alert("Miss!")
+        alert("Miss!");
     }
 }
 
-var accuracy = (guess > 0 ? (hits / guesses) * 100 : 0). toFixed(3);
-var stats = "You took " +guesses + " guesses to sink the battleship! " + "\n" + "Accuracy: " + accuracy + "%";
-var stats = "You sank my battleship at: " + guesses + "guesses" + "and your accuracy is: " + (3/guesses);
+var accuracy = (guesses > 0 ? (hits / guesses) * 100 : 0).toFixed
+(2);
+var stats = "You took" + guesses + "guesses to sink the battleship!\n " + "Accuracy" + accuracy + "%";
+alert(stats);
